fe2182179983a3d79b9e759d135ab5854f965f39
AlexYavorova <5rova.alex@gmail.com>
2026-08-05 19:07:45 +0300

    Import button
    Needs a db table to write to

--- ICard API Project/Form1.Designer.cs
+++ ICard API Project/Form1.Designer.cs
@@ -33,13 +33,15 @@
             label1 = new Label();
             button2 = new Button();
             button3 = new Button();
+            button4 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(223, 138);
+            button1.Location = new Point(255, 184);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 0;
             button1.Text = "Endpoint 1";
             button1.UseVisualStyleBackColor = true;
@@ -47,25 +49,27 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(294, 99);
+            textBox1.Location = new Point(336, 132);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(317, 81);
+            label1.Location = new Point(362, 108);
             label1.Name = "label1";
-            label1.Size = new Size(56, 15);
+            label1.Size = new Size(73, 20);
             label1.TabIndex = 2;
             label1.Text = "Device ID";
             // 
             // button2
             // 
-            button2.Location = new Point(304, 138);
+            button2.Location = new Point(347, 184);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(86, 31);
             button2.TabIndex = 3;
             button2.Text = "Endpoint 2";
             button2.UseVisualStyleBackColor = true;
@@ -73,24 +77,37 @@
             // 
             // button3
             // 
-            button3.Location = new Point(385, 138);
+            button3.Location = new Point(440, 184);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(86, 31);
             button3.TabIndex = 4;
             button3.Text = "Endpoint 3";
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
+            // button4
+            // 
+            button4.Location = new Point(347, 269);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 5;
+            button4.Text = "Import";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -104,5 +121,6 @@
         private Label label1;
         private Button button2;
         private Button button3;
+        private Button button4;
     }
 }
--- ICard API Project/Form1.cs
+++ ICard API Project/Form1.cs
@@ -1,6 +1,8 @@
 using ICard_API_Project.Models;
 using Microsoft.Extensions.Configuration;
 using System.Configuration;
+using System.IO;
+using System.Text;
 using System.Text.Json;
 
 namespace ICard_API_Project
@@ -19,7 +21,7 @@ namespace ICard_API_Project
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            
+
             if (textBox1.Text == String.Empty)
             {
                 MessageBox.Show("You must enter a device id first!");
@@ -53,10 +55,10 @@ namespace ICard_API_Project
                 return;
             }
             string endpoint = textBox1.Text + _configuration["Endpoints:DeviceLocation"];
-            
+
             DeviceLocation locations = new DeviceLocation();
 
-            while(locations.lastPage == false) //Multiple pages not tested yet
+            while (locations.lastPage == false) //Multiple pages not tested yet
             {
                 string jsonResult = await sendGetRequest(endpoint + $"?pageNumber={locations.pageNumber}");
                 DeviceLocation? currentPage = null;
@@ -71,13 +73,44 @@ namespace ICard_API_Project
             }
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "CSV Files (*.csv)|*.csv|Excel Files (*.xls)|*.xls";
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = dialog.FileName;
+                List<string> firstColumn = new List<string>();
+
+                using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read), Encoding.UTF8))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+
+                        // Split the row by comma and take the first item (index 0)
+                        string[] columns = line.Split(',');
+                        if (columns.Length > 0)
+                        {
+                            firstColumn.Add(columns[0].Trim());
+                        }
+                    }
+                }
+
+                // Example output: verify how many items were read
+                MessageBox.Show($"Successfully read {firstColumn.Count} items from column 1.");
+            }
+        }
 
         private async Task<string> sendGetRequest(string endpoint)
         {
             try
             {
                 var client = _httpClientFactory.CreateClient("ICardApiClient");
-                
+
                 HttpResponseMessage response = await client.GetAsync(endpoint);
 
                 response.EnsureSuccessStatusCode();
